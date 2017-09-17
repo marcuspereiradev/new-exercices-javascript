123ab38a9bc9d1ebacb4fbe2663486f985c0457c
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-17 16:56:35 -0300

    Sum of two numbers

---
+++ exercicio_1.js
@@ -0,0 +1,11 @@
+
+    
+    // Soma de dois números //
+
+var num1 = parseInt(prompt("Informe o primeiro valor: "));
+var num2 = parseInt(prompt("Informe o segundo valor: "));
+var resultado = num1 + num2
+
+            
+alert("Soma: " + resultado)
+
