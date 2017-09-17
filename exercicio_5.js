3d1cbc20e627f9c6c5bcca947e580960a7bfdc3e
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-17 17:04:38 -0300

    average in the semester

---
+++ exercicio_5.js
@@ -0,0 +1,13 @@
+
+
+// Média no Semestre //
+
+    var nome = prompt("Informe o nome do aluno");
+    var nota1 = parseInt(prompt("Informe a primeira nota"));
+    var nota2 = parseInt(prompt("Informe a segunda nota"));
+    var nota3 = parseInt(prompt("Informe a terceira nota"));
+    var media = (nota1 + nota2 + nota3) / 3;
+    
+    alert("Nome do aluno: " + nome)
+    alert("Média das notas: " + media)
+    
