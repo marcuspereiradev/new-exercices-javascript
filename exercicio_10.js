f3214d15a744480a82a44ee7aef4101655e9a600
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-17 18:33:43 -0300

    Shop Papaya with Sugar

---
+++ exercicio_10.js
@@ -0,0 +1,12 @@
+
+
+
+// Loja Mamão com açúcar 
+
+
+    var valorProduto = parseFloat(prompt("Digite o valor do produto."));
+    var prestacao = 5;
+    var valorPrestacao = valorProduto / prestacao;
+    
+    alert ("Prestação em 5x sem juros = R$ " + valorPrestacao);
+    
