ce43b1c7054ba0fba766ec58ed1389a380e6f5c1
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-17 17:02:07 -0300

    Average fuel consumption

---
+++ exercicio_3.js
@@ -0,0 +1,11 @@
+
+    
+    // Consumo médio de gasolina //
+
+var distanciaTotal = parseInt(prompt("Digite o valor da distância percorrida: "));
+var combustivelGasto = parseInt(prompt("Digite o valor do combustível gasto: "));
+
+var comsumoMedio = distanciaTotal / combustivelGasto 
+            
+alert("Consumo médio é: " + comsumoMedio + "KM/L")
+
