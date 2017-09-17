a19a067e8fa9b425d30bf0e549b19b339f286d46
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-17 18:27:58 -0300

    Celsius to Fahrenheit

---
+++ exercicio_7.js
@@ -0,0 +1,9 @@
+
+
+// Celsius para Fahrenheit //
+
+    
+    var C = parseInt(prompt("Informe a temperatura em Celsius"))
+    var F = (9 * C + 160) / 5
+    
+    alert(C + " Graus Celsius é igual a " + F + " Fahrenheit")
