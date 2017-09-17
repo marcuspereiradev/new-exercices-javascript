2a34723105c77e036866b480239fa57f41cd62f5
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-17 17:05:44 -0300

    changed values

---
+++ exercicio_6.js
@@ -0,0 +1,12 @@
+
+
+// Valores  trocados //
+    
+    
+    var A = parseInt(prompt("Informe o valor de A"))
+    var B = parseInt(prompt("Informe o valor de B"))
+    
+    
+    alert("Valor de A = " + B) 
+    alert("Valor de B = " + A)
+
