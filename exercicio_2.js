d45fe67f8a01e9e7d83bb8842759bf67bb554dad
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-17 17:00:12 -0300

    all arithmetic values on the screen

---
+++ exercicio_2.js
@@ -0,0 +1,18 @@
+
+    
+    // todos os valores aritméticos na tela //
+
+var num1 = parseInt(prompt("Informe o primeiro valor: "));
+var num2 = parseInt(prompt("Informe o segundo valor: "));
+
+var resultadoSoma = num1 + num2
+var resultadoSubtracao = num1 - num2
+var resultadoMultiplicacao = num1 * num2
+var resultadoDivisao = num1 / num2
+
+            
+alert("Soma: " + resultadoSoma)
+alert("Subtração: " + resultadoSubtracao)
+alert("Multiplicação: " + resultadoMultiplicacao)
+alert("Divisão: " + resultadoDivisao)
+
