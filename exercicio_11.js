2a45f755a3df4d0ade7a70b7484d9d8978a9724d
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-17 18:36:28 -0300

    Return

---
+++ exercicio_11.js
@@ -0,0 +1,10 @@
+
+
+// Lucro
+
+
+    var precoCustogasolina = parseFloat(prompt("Informe o preço de custo da gasolina."));
+    var acrescimoLucro = parseFloat(prompt("Informe o acréscimo."));
+    var valorVenda = precoCustogasolina + acrescimoLucro;
+    
+    alert ("Valor de venda é: R$ " + valorVenda);
