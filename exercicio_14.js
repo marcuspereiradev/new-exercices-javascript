2f7d55ab755a07da634927f113981f507aff80d0
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-18 10:36:54 -0300

    Who is the bigger

---
+++ exercicio_14.js
@@ -0,0 +1,16 @@
+
+
+// Quem é o maior 
+
+
+
+    var valorx = parseInt(prompt("Digite o primeiro valor"));
+    var valory = parseInt(prompt("Digite o segundo valor"));
+    
+    if  (valorx > valory) {
+        alert ("Valor X(primeiro) é o maior número: " + valorx);
+    }
+    
+    else {
+            alert ("Valor Y(segundo) é o maior número: " + valory);
+        }
