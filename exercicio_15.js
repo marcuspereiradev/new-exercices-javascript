bc939762c0da29a1d8edd33220389bf87047848a
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-18 10:57:11 -0300

    Interval between 100 and 200

---
+++ exercicio_15.js
@@ -0,0 +1,16 @@
+
+
+
+// Intervalo de 100 a 200 
+
+
+    var valor = parseInt(prompt("Informe um número"));
+    
+    if (valor >= 100 && valor <= 200) {
+        alert (valor + " está no intervalo entre 100 e 200!")
+    }
+    
+    else {
+        alert (valor + " NÃO está no intervalo entre 100 e 200!")
+    }
+    
