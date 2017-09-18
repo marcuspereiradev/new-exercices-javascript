fe557aa3e4ba9241ff3a25106c9fe2c7b4ccc292
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-18 17:37:47 -0300

    over 18 years old

---
+++ exercicio_18.js
@@ -0,0 +1,14 @@
+
+// Maior de idade
+
+for (var x=1; x<=75; x++){
+    var idade = parseInt(prompt("Informe a idade"));
+    
+    if(idade >= 18){
+        alert("Maior de idade!");
+    }
+    
+    else{
+        alert("Menor de idade")
+    }
+}
