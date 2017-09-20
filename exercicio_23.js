0be41921a1a3bc61c1550c92ad14d1396da1554c
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-19 22:26:46 -0300

    bigger, smaller or equal

---
+++ exercicio_23.js
@@ -0,0 +1,15 @@
+
+// Maior, menor ou igual
+
+	var valor = prompt("Digite um valor");
+	
+	if (valor > 80){
+		alert(valor + " é maior que 80!");
+	}
+	if (valor < 25){
+		alert(valor + " é menor que 25!");
+	}
+	if (valor == 40){
+		alert(valor + " é igual a 40!")
+	}
+	
