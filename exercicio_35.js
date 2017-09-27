032c6bc2d75efbfac27c496f78eed430f4f72e14
marcuspereiragithub <vinnycoast@gmail.com>
2017-09-27 15:02:57 -0300

    calculating expenses

---
+++ exercicio_35.js
@@ -0,0 +1,10 @@
+
+// Calculando gastos
+	
+	var despesaCategoria = 1532;
+	var rendaTotal = 6255;
+
+	var porcentagemCategoria = (despesaCategoria/rendaTotal) * 100;
+
+	console.log(porcentagemCategoria.toFixed(1));
+
